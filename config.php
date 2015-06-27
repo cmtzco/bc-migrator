2c7753a3634e111389661f315d25c8e645b8ba34
Chris Martinez <cmtz@Chriss-MacBook-Pro.local>
2015-06-26 21:52:26 -0500

    Fixed file structure

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+require 'migrator.php';
+
+define('DB_HOST', 'scrmysql02.scripti.dc');
+define('DB_DATABASE_NAME', 'rlm');
+define('DB_USERNAME', 'rlm');
+define('DB_PASSWORD', 'fn226QHycZLbM3Xj');
+
+//set credentials 
+$storeUrl = 'https://store-f50y1r.mybigcommerce.com/';
+$username = 'scriptilabs';
+$apiKey = 'c4709e020662fe501d6a707c7e507dc4c7225d1a';
+
+
